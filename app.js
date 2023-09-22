56368ec1f0348e5f2a1f6872a249536bce76d75c
Donner-von-Swabia <severett21@ivytech.edu>
2023-09-22 12:15:34 -0400

    style changes

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ const app = express();
 app.set('view engine','ejs');
 app.listen(3000);
 app.get('/',(req,res)=>{
-    res.render('index',{title:'Home'})
+    res.render('index',{title:'Home',siteName:'A Class Coding'})
+});
+
+app.get('/teacher',(req,res)=>{
+    res.render('teacher',{title:'Teacher',siteName:'A Class Coding: Teachers'})
+});
+
+app.get('/student',(req,res)=>{
+    res.render('student',{title:'Student',siteName:'A Class Coding: Students'})
+});
+
+app.get('/super',(req,res)=>{
+    res.render('super',{title:'Super',siteName:'Super'})
 });
 
 app.use((req,res)=> {
-    res.status(404).render('404',{title:'404:Page Not Found'})
+    res.status(404).render('404',{title:'404:Page Not Found',siteName:'404:Page Not Found'})
 });
